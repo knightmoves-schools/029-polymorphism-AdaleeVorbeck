fe30bfac8a7a987307144a1d59ef1f794c688174
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-04-30 19:23:53 +0000

    NOAH RELEASED A NEW ALBUM SO YOU KNOW THIS IS GONNA BE BOMB

--- Animals.cs
+++ Animals.cs
@@ -1,5 +1,9 @@
 ﻿namespace knightmoves;
 
+//should rename the Talk method on the Dog class to Say
+//should rename the Sing method on the Bird class to Say
+//should replace the if statements with a single call to the Say method on each of the animals
+
 public class Trainer{
     public string Listen(Animal[] animals) {
         string sounds = "";
