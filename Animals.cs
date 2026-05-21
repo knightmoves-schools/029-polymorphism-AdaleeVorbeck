e9e839873a98b266538e763c22053849596016c7
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-05-21 19:13:46 +0000

    Quite a few kids just got DQed

--- Animals.cs
+++ Animals.cs
@@ -1,39 +1,49 @@
 ﻿namespace knightmoves;
 
-//should rename the Talk method on the Dog class to Say 
-//should rename the Sing method on the Bird class to Say 
-//should replace the if statements with a single call to the Say method on each of the animals
+// should rename the Talk method on the Dog class to Say
+// should rename the Sing method on the Bird class to Say
+// should replace the if statements with a single call to the Say method on each of the animals
 
-public class Trainer{
-    public string Listen(Animal[] animals) {
+public class Trainer
+{
+    public string Listen(Animal[] animals)
+    {
         string sounds = "";
 
-        foreach(Animal animal in animals){
+        foreach (Animal animal in animals)
+        {
             sounds += animal.Say() + ", ";
         }
 
-        return sounds;
+        return sounds.TrimEnd(',', ' ');
     }
 }
 
-public abstract class Animal {
+public abstract class Animal
+{
     public abstract string Say();
 }
 
-public class Cat : Animal{
-    public override string Say() {
+public class Cat : Animal
+{
+    public override string Say()
+    {
         return "meow";
     }
 }
 
-public class Dog : Animal{
-    public override string Say() {
+public class Dog : Animal
+{
+    public override string Say()
+    {
         return "woof";
     }
 }
 
-public class Bird : Animal{
-    public override string Say() {
+public class Bird : Animal
+{
+    public override string Say()
+    {
         return "chirp";
     }
 }
