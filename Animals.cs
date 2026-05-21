13d0962d81e905cac97f89c7937354ed9128dfe6
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-05-21 19:11:30 +0000

    We watching track right now

--- Animals.cs
+++ Animals.cs
@@ -1,7 +1,7 @@
 ﻿namespace knightmoves;
 
-//should rename the Talk method on the Dog class to Say
-//should rename the Sing method on the Bird class to Say
+//should rename the Talk method on the Dog class to Say 
+//should rename the Sing method on the Bird class to Say 
 //should replace the if statements with a single call to the Say method on each of the animals
 
 public class Trainer{
@@ -9,18 +9,7 @@ public class Trainer{
         string sounds = "";
 
         foreach(Animal animal in animals){
-            if(typeof(Cat).IsInstanceOfType(animal))
-            {
-                sounds += ((Cat) animal).Say() + ", ";
-            } 
-            else if (typeof(Dog).IsInstanceOfType(animal))
-            {   
-                sounds += ((Dog) animal).Talk() + ", ";
-            } 
-            else if (typeof(Bird).IsInstanceOfType(animal))
-            {
-                sounds += ((Bird) animal).Sing() + ", ";
-            }
+            sounds += animal.Say() + ", ";
         }
 
         return sounds;
@@ -28,24 +17,23 @@ public class Trainer{
 }
 
 public abstract class Animal {
-    
+    public abstract string Say();
 }
 
 public class Cat : Animal{
-    public string Say() {
+    public override string Say() {
         return "meow";
     }
 }
 
 public class Dog : Animal{
-    public string Talk() {
+    public override string Say() {
         return "woof";
     }
 }
 
-public class Bird  : Animal{
-    public string Sing() {
+public class Bird : Animal{
+    public override string Say() {
         return "chirp";
     }
 }
-
